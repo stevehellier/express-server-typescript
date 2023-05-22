897d9a2cdbd64879990b7ef4cea64f964fd3c834
Steve Hellier <steve_hellier@hotmail.com>
2023-05-22 22:42:29 +0100

    Added basic caching

--- src/api/weather/getWeather.ts
+++ src/api/weather/getWeather.ts
@@ -1,4 +1,4 @@
-import { Response, Request, RequestHandler } from 'express';
+import { Response, Request, RequestHandler, NextFunction } from 'express';
 import getRndNumber from '../../helpers/numbers';
 
 interface Weather {
@@ -6,6 +6,11 @@ interface Weather {
   temperatureC: number;
   temperatureF: number;
   summary: string;
+}
+
+interface CachedWeather {
+  weather: Array<Weather>;
+  cachedTime: number;
 }
 
 const summaries = [
@@ -20,26 +25,48 @@ const summaries = [
   'Sweltering',
   'Scorching',
 ];
+const cachedData: CachedWeather = {
+  weather: [],
+  cachedTime: 0,
+};
+let cachedTime = 0;
 
-const getWeather: RequestHandler = (req: Request, res: Response) => {
-  const weather: Array<Weather> = [];
-
-  for (let index = 0; index < 5; index += 1) {
-    const tempC = Number(`${getRndNumber(-5, 20).toFixed(2)}`);
-    const tempF = Number((32 + Number(tempC) / 0.556).toFixed(2));
-    const date = new Date();
-    date.setDate(date.getDate() + index);
-    const item = {
-      dateFormated: `${date.toLocaleDateString()}`,
-      temperatureC: tempC,
-      temperatureF: tempF,
-      summary: `${
-        summaries[Math.floor(Number(getRndNumber(0, summaries.length - 1)))]
-      }`,
-    };
-    weather.push(item);
+const getWeather: RequestHandler = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  if (cachedTime && cachedTime > Date.now() - 30 * 1000) {
+    return res.json(cachedData);
   }
-  return res.json(weather);
+
+  try {
+    const weather: Array<Weather> = [];
+
+    for (let index = 0; index < 5; index += 1) {
+      const tempC = Number(`${getRndNumber(-5, 20).toFixed(2)}`);
+      const tempF = Number((32 + Number(tempC) / 0.556).toFixed(2));
+      const date = new Date();
+      date.setDate(date.getDate() + index);
+      const item = {
+        dateFormated: `${date.toLocaleDateString()}`,
+        temperatureC: tempC,
+        temperatureF: tempF,
+        summary: `${
+          summaries[Math.floor(Number(getRndNumber(0, summaries.length - 1)))]
+        }`,
+      };
+      weather.push(item);
+    }
+
+    cachedTime = Date.now();
+
+    cachedData.weather = weather;
+    cachedData.cachedTime = cachedTime;
+    return res.json(cachedData);
+  } catch (error) {
+    return next(error);
+  }
 };
 
 export default getWeather;
--- src/middlewares/logging/logger.ts
+++ src/middlewares/logging/logger.ts
@@ -1,11 +1,12 @@
 import { Response, Request, NextFunction } from 'express';
 
 const logger = (req: Request, res: Response, next: NextFunction) => {
-  const { headers, url, baseUrl } = req;
+  const { headers, url, baseUrl, method } = req;
   const log = {
     headers,
     url,
     baseUrl,
+    method,
   };
   console.log(log);
   return next();
