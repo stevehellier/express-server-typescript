0766298004b31b7c55cbe01005aa98d66e5a719c
Steve Hellier <steve_hellier@hotmail.com>
2023-05-24 13:19:07 +0100

    Update to check for missing headers

--- src/middlewares/authencation/authencation.ts
+++ src/middlewares/authencation/authencation.ts
@@ -1,6 +1,12 @@
 import { Response, Request, NextFunction } from 'express';
 
 const authentication = (req: Request, res: Response, next: NextFunction) => {
+  if (!req.headers || req.headers === null) {
+    const error = Error('Missing headers');
+    res.status(500);
+    return next(error);
+  }
+
   const b64auth = (req.headers.authorization || '').split(' ')[1] || '';
   const [login, password] = Buffer.from(b64auth, 'base64')
     .toString()
